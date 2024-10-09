aa9cb0250f20903308690bac2be42edbfbe4b6a1
AlaaAlzahrani <alzahrani.alaaa@gmail.com>
2024-10-09 09:07:55 +0300

    updated hooks

--- components/hooks/use-toast.ts
+++ components/hooks/use-toast.ts
@@ -1,6 +1,5 @@
 "use client"
 
-// Inspired by react-hot-toast library
 import * as React from "react"
 
 import type {
@@ -18,12 +17,16 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
+// Define actionTypes as a const object
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
   REMOVE_TOAST: "REMOVE_TOAST",
-} as const
+} as const;
+
+// Create a type from the values of actionTypes
+type ActionType = typeof actionTypes[keyof typeof actionTypes];
 
 let count = 0
 
@@ -32,23 +35,21 @@ function genId() {
   return count.toString()
 }
 
-type ActionType = typeof actionTypes
-
 type Action =
   | {
-      type: ActionType["ADD_TOAST"]
+      type: "ADD_TOAST"
       toast: ToasterToast
     }
   | {
-      type: ActionType["UPDATE_TOAST"]
+      type: "UPDATE_TOAST"
       toast: Partial<ToasterToast>
     }
   | {
-      type: ActionType["DISMISS_TOAST"]
+      type: "DISMISS_TOAST"
       toastId?: ToasterToast["id"]
     }
   | {
-      type: ActionType["REMOVE_TOAST"]
+      type: "REMOVE_TOAST"
       toastId?: ToasterToast["id"]
     }
 
