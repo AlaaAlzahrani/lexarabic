ca742bee6effe3bd29d18e72d87545a673cbc1d2
AlaaAlzahrani <alzahrani.alaaa@gmail.com>
2024-10-09 09:28:36 +0300

    added website

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   const header = (
     <header className="p-5 sm:p-8">
       <div className="flex items-center justify-between">
-        <Link href="/" className='hover:opacity-80'>
+        <Link href="https://lexarabic.netlify.app/" className='hover:opacity-80'>
           <Image 
             src="/lexarabic_logo.svg"
             alt="LexArabic Logo"
@@ -44,7 +44,7 @@ export default function RootLayout({
   <footer className="bg-white dark:bg-gray-900">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
-              <a href="http://localhost:3000/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+              <a href="https://lexarabic.netlify.app/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                   <Image src="/lexarabic_logo.svg" 
                   className="h-8" 
                   alt="شعار كَلِم"
@@ -54,23 +54,23 @@ export default function RootLayout({
               </a>
               <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                   <li>
-                      <a href="/about" className="hover:text-[#8f5ecc] me-4 md:me-6">عن الموقع</a>
+                      <a href="https://lexarabic.netlify.app/about" className="hover:text-[#8f5ecc] me-4 md:me-6">عن الموقع</a>
                   </li>
                   <li>
-                      <a href="/privacy" className="hover:text-[#8f5ecc] me-4 md:me-6">الخصوصية</a>
+                      <a href="https://lexarabic.netlify.app/privacy" className="hover:text-[#8f5ecc] me-4 md:me-6">الخصوصية</a>
                   </li>
                   <li>
-                      <a href="/license" className="hover:text-[#8f5ecc] me-4 md:me-6">الترخيص</a>
+                      <a href="https://lexarabic.netlify.app/license" className="hover:text-[#8f5ecc] me-4 md:me-6">الترخيص</a>
                   </li>
                   <li>
-                      <a href="/contact" className="hover:text-[#8f5ecc]">تواصل معنا</a>
+                      <a href="https://lexarabic.netlify.app/contact" className="hover:text-[#8f5ecc]">تواصل معنا</a>
                   </li>
               </ul>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
               <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                  © 2024 <a href="http://localhost:3000/" className="hover:text-[#8f5ecc]">كَلِم</a>. جميع الحقوق محفوظة.
+                  © 2024 <a href="https://lexarabic.netlify.app/" className="hover:text-[#8f5ecc]">كَلِم</a>. جميع الحقوق محفوظة.
               </span>
               <div className="flex mt-4 sm:justify-center sm:mt-0">
                   <a href="https://github.com/AlaaAlzahrani/lexarabic" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
